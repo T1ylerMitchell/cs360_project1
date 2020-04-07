c51dc34a5720b94a797c96e6dd5686d7e3551d44
Haider Ali <56099752+hydencoder@users.noreply.github.com>
2020-04-06 21:07:56 -0400

    Add files via upload

---
+++ excel_f.php
@@ -0,0 +1,21 @@
+<?php
+  include_once "SimpleXLSX.php";
+  include_once "db_connect.php";
+  
+  $excel = $_POST['excel'];
+
+  if ($xlsx = SimpleXLSX::parse($excel)) {
+
+    foreach ($xlsx->rows() as $elt) { 
+      $query = "INSERT INTO product(name, description, price, image) VALUE('$elt[0]', '$elt[1]', '$elt[2]', '$elt[3]');";
+      $qRes1 = $db->query($query);
+
+      if($qRes1 == false) {
+        echo "<h3>Failed to insert: $elt[0]<h3>";
+      }
+    } 
+
+  } else {
+    echo SimpleXLSX::parseError();
+  }
+?>
